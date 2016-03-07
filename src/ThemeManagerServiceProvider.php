ad2014a984178703e1ecf9018cc362d695f5c50a
Bryan Salazar <brysalazar12@gmail.com>
2016-03-07 16:37:23 +0800

    Update ThemeManagerServiceProvider.php

--- src/ThemeManagerServiceProvider.php
+++ src/ThemeManagerServiceProvider.php
@@ -4,7 +4,6 @@ use Illuminate\Support\ServiceProvider;
 use Mirage\ThemeManager\Helpers\Contracts\ThemeContract;
 use Mirage\ThemeManager\Helpers\Theme;
 use Illuminate\View\FileViewFinder;
-use Illuminate\View\Factory;
 use Mirage\ThemeManager\Events\ThemeViewEvent;
 
 /**
